1c5131dedeafe4754af00937e97f422118582454
Arj09 <arjunkushwah100199@gmail.com>
2026-04-21 02:09:28 +0530

    change package

--- src/Component/Http.jsx
+++ src/Component/Http.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const Http = axios.create({
-   //baseURL : "http://localhost:5000"
-   baseURL:'https://store-backend-o5qm.onrender.com',
+   baseURL : "http://localhost:5000"
+   //baseURL:'https://store-backend-o5qm.onrender.com',
    
    
 })
